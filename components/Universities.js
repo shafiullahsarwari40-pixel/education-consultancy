3f360de6acc8817faaccdd2a0695054828e69207
Education Consultancy Deployment <deployment@education-consultancy.local>
2026-06-14 02:35:01 +0300

    Update Turkish university logos and universities section

--- components/Universities.js
+++ components/Universities.js
@@ -4,84 +4,84 @@ import Image from 'next/image';
 
 const universities = [
   {
-    slug: 'adiyaman-university',
+    logo: '/images/universities/adiyaman-university.png',
     name: 'Adıyaman University',
     city: 'Adıyaman',
     programs: '100+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'ankara-yildirim-beyazit-university',
+    logo: '/images/universities/ankara-yildirim-beyazit-university.svg',
     name: 'Ankara Yıldırım Beyazıt University',
     city: 'Ankara',
     programs: '160+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'burdur-mehmet-akif-ersoy-university',
+    logo: '/images/universities/burdur-mehmet-akif-ersoy-university.png',
     name: 'Burdur Mehmet Akif Ersoy University',
     city: 'Burdur',
     programs: '90+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'kirikkale-university',
+    logo: '/images/universities/kirikkale-university.svg',
     name: 'Kırıkkale University',
     city: 'Kırıkkale',
     programs: '110+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'duzce-university',
+    logo: '/images/universities/duzce-university.svg',
     name: 'Düzce University',
     city: 'Düzce',
     programs: '95+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'zonguldak-bulent-ecevit-university',
+    logo: '/images/universities/zonguldak-bulent-ecevit-university.png',
     name: 'Zonguldak Bülent Ecevit University',
     city: 'Zonguldak',
     programs: '120+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'kastamonu-university',
+    logo: '/images/universities/kastamonu-university.jpg',
     name: 'Kastamonu University',
     city: 'Kastamonu',
     programs: '80+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'usak-university',
+    logo: '/images/universities/usak-university.png',
     name: 'Uşak University',
     city: 'Uşak',
     programs: '85+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'izmir-katip-celebi-university',
+    logo: '/images/universities/izmir-katip-celebi-university.png',
     name: 'İzmir Katip Çelebi University',
     city: 'İzmir',
     programs: '130+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'mersin-university',
+    logo: '/images/universities/mersin-university.svg',
     name: 'Mersin University',
     city: 'Mersin',
     programs: '100+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'ondokuz-mayis-university',
+    logo: '/images/universities/ondokuz-mayis-university.png',
     name: 'Ondokuz Mayıs University',
     city: 'Samsun',
     programs: '140+ Programs',
     languages: 'English, Turkish',
   },
   {
-    slug: 'anadolu-university',
+    logo: '/images/universities/anadolu-university.svg',
     name: 'Anadolu University',
     city: 'Eskişehir',
     programs: '180+ Programs',
@@ -104,10 +104,11 @@ export default function Universities() {
             <article key={uni.slug} className="university-card">
               <div className="university-logo">
                 <Image
-                  src={`/images/universities/${uni.slug}.svg`}
+                  src={uni.logo}
                   alt={`${uni.name} logo`}
                   width={280}
                   height={98}
+                  priority={false}
                 />
               </div>
 
